e86d5ecb42be1d8c427fb90de54f6f66d8d6f0de
AghilasKessai <56322894+AghilasClipher@users.noreply.github.com>
2020-01-17 15:53:55 +0100

    Update Ecran2.java
    Ici, on peut choisir d'afficher la carte directement, d’accéder au tutorial, ou de configurer l'application.

--- src/Ecran2.java
+++ src/Ecran2.java
@@ -1,15 +1,21 @@
+
 package com.example.findpt;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class Ecran2 extends AppCompatActivity implements LocationListener {
@@ -26,11 +32,45 @@ public class Ecran2 extends AppCompatActivity implements LocationListener {
     @Override
     protected void onResume() {
         super.onResume();
-        checkPermissions();
+
+        Button button_carte=findViewById(R.id.button_carte);
+        Button button_tutoriel=findViewById(R.id.button_tutoriel);
+        Button button_config=findViewById(R.id.button_config);
+        //Lorsque on appuye sur le bouton 'Tutoriel'
+        button_tutoriel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ouvrirTutoActivity();
+            }
+        });
+        // Lorsque on appuye sur le bouton 'Afficher la carte' alors le code appelle la methode CheckPermissions.
+        button_carte.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                checkPermissions();
+            }
+        });
+        //Lorsque on appuye sur le bouton 'Configurqtion' on accede a ConfigActivity
+        button_config.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ouvrirConfigActivity();
+            }
+        });
         //Les request LOCATION UPDATE nécessitent que l'utilisateur ait autorisé l'application a accéder a sa localisation.
 
     }
+    public void ouvrirConfigActivity(){
+        Intent intent = new Intent(this,ConfigActivity.class);
+        startActivity(intent);
+    }
+    public void ouvrirTutoActivity(){
+        Intent intent = new Intent(this,TutorielActivity.class);
+        startActivity(intent);
+    }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     private void checkPermissions(){
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -40,23 +80,17 @@ public class Ecran2 extends AppCompatActivity implements LocationListener {
             );
             return;
         }
-        // A partir d'ici, on sait que l'utilisateur a autorisé l'app a accéder a la localisation. On affiche un message (en Toast).
-        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
-        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            // Demande sur le systeme d'une nouvelle localisation chaque 10 secondes (10000ms).
 
-            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, this);
-        }
-        if(lm.isProviderEnabled(LocationManager.PASSIVE_PROVIDER)){
-            // Demande sur le systeme d'une nouvelle localisation chaque 10 secondes (10000ms).
-            lm.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER,10000,0,this);
-        }
-        if(lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
-            // Demande sur le systeme d'une nouvelle localisation chaque 10 secondes (10000ms).
-            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,10000,0,this);
-        }
+        // A partir d'ici, on sait que l'utilisateur a autorisé l'app a accéder a la localisation. On affiche un message (en Toast).
+        Toast.makeText(Ecran2.this,"Vous avez autorisé FINDP&T à acceder à votre localisation.",Toast.LENGTH_SHORT).show();
+        // On ouvre donc l'activity de la carte Google Maps.
+        Intent intent = new Intent(this,MapsActivity.class);
+        startActivity(intent);
+        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
